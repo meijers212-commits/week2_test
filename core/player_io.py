7d84ded8a2a2ec83aa9014cea4bde3b4ba57bde1
meijers212-commits <meijers212@gmail.com>
2025-10-30 12:43:44 +0200

    Refactor player action input validation
    fixing ask_player_action function

--- core/player_io.py
+++ core/player_io.py
@@ -2,7 +2,7 @@ def ask_player_action() -> str:
     while True:
         choice = input("please choose 'H 'for 'hit' or S for 'stand'")
         choice = choice.lower()
-        if choice == "h" or "s":
+        if choice in ["h","s"]:
             break
     return choice
 
